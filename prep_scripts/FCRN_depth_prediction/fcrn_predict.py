b898057c2b249f95ed517364fded5631960eeae8
Nikolai Gaiduchenko <Gaiduchenko.NE@gmail.com>
2018-11-02 13:28:13 +0300

    adapted fcrn (beta)

--- prep_scripts/FCRN_depth_prediction/fcrn_predict.py
+++ prep_scripts/FCRN_depth_prediction/fcrn_predict.py
@@ -1,44 +1,55 @@
-import argparse
+# System imports
 import os
+import os.path as osp
+
+# Third-party imports
+import argparse
+import cv2
+import h5py
+import matplotlib.pyplot as plt
 import numpy as np
 import tensorflow as tf
 
-import matplotlib.pyplot as plt
-import cv2
+# Local imports
 import models
 
+# Define default constants
+FCRN_MODEL_PATH = './models/NYU_FCRN.ckpt'
+FCRN_INPUT_PATH = '../../data/images/raw'
+FCRN_OUTPUT_PATH = '../../data/images/depths'
+FCRN_IMAGE_SHAPE = (1024, 768) # height, width in pixels
+
 class FCRNDepthPredictor:
-    def __init__(self, model_path=None, image_path=None, pred_dir_flag=None,
+    def __init__(self, path_model=FCRN_MODEL_PATH,
                  image_shape=None, channels=3, batch_size=1, verbose=False):
         '''Construct TensorFlow graph to predict depth in the future.
 
         Args:
-            model_path (str):
-                the path to pretrained model weights
+            path_model (str):
+                path to pretrained model weights
 
-            image_path (str):
-                the path to the image the depth need to be estimated to
+            path_input (str):
+                path to the directory with input raw RGB images
 
-            pred_dir_flag (bool):
-                toggle predict depth in all images in the directory
+            path_output (str):
+                path to the directory the depth dataset will be stored into
 
             channels (int):
                 number of image channels, such as RGB (channels == 3)
 
             batch_size (int):
-                the size of the batch size for ResNet
+                size of the batch size for ResNet
 
             verbose (bool):
                 toggle print debug information to the console
         '''
         self.verbose = verbose
-        self.model_path = model_path
-        self.image_path = image_path
-        self.pred_dir_flag = pred_dir_flag
+        self.path_model = path_model
 
         # Remember tensorflow session
         self.batch_size = batch_size
         self.channels = channels
+        self.saver = None
 
         # Create a placeholder for the input image
         self.input_node = tf.placeholder(tf.float32, shape=(None, image_shape[0], image_shape[1], channels))
@@ -56,26 +67,25 @@ class FCRNDepthPredictor:
         '''Load model's pretrained weights'''
         # Use to load from ckpt file
         self.saver = tf.train.Saver()
-        self.saver.restore(sess, self.model_path)
+        self.saver.restore(sess, self.path_model)
 
         # Use to load from npy file
         #net.load(model_data_path, sess)
 
-
-    def load_image(self, image_path, height=1024, width=768):
-        '''Load and preprocess image from `image_path` given.'''
-
+    def load_image(self, path_input, shape=FCRN_IMAGE_SHAPE):
+        '''Load and preprocess image from `path_input` given.'''
         # Load original image
-        if not os.path.exists(image_path):
+        if not os.path.exists(path_input):
             if self.verbose:
-                print(f'[FCRN]: Image {image_path} was not found!')
+                print(f'[FCRN]: Image {path_input} was not found!')
             raise FileNotFoundError
         else:
-            image_cv2 = cv2.imread(image_path)
+            image_cv2 = cv2.imread(path_input)
         if self.verbose:
             print(f'[FCRN]: Original image loaded with shape: {image_cv2.shape}')
 
         # Resize image to the requested shapes
+        height, width = shape[0], shape[1]
         if height is None:
             height = image_cv2.shape[0]
         if width is None:
@@ -92,33 +102,75 @@ class FCRNDepthPredictor:
             print(f'[FCRN]: Postprocessed image shape: {image_cv2.shape}')
         return image_cv2
 
-    def save_depth(self, depth, path):
-        '''Save depth as numpy pickle and as image'''
-        fig = plt.figure()
-        ii = plt.imshow(depth[0,:,:,0], interpolation='nearest')
-        fig.colorbar(ii)
-        plt.savefig('%s.png' % path)
-        np.save('%s.npy' % path, depth)
+    # def save_depth(self, depth, path):
+    #     '''DEPRICATED: Save depth as numpy pickle and as image'''
+    #     fig = plt.figure()
+    #     ii = plt.imshow(depth[0, :, :, 0], interpolation='nearest')
+    #     fig.colorbar(ii)
+    #     plt.savefig('%s.png' % path)
+    #     np.save('%s.npy' % path, depth)
 
-    def predict_dir(self, model_path, dir_path):
-        '''Predict depth for all images in the dir_path'''
-        img_fnames = os.listdir(dir_path)
+    def predict_dir(self, path_input=FCRN_INPUT_PATH, path_output=FCRN_OUTPUT_PATH, sess=None):
+        '''Predict depth for all images in the path_input'''
+        if not os.path.exists(path_input):
+            if self.verbose:
+                print(f'[FCRN]: Image input directory {path_input} was not found!')
+            raise NotADirectoryError
 
-        for path in img_fnames:
-            image_path = os.path.join(dir_path, path)
-            image = self.load_image(image_path)
-            pred = self.predict(model_path, image)
-            self.save_depth(pred, image_path + '_depth')
+        # load images
+        files = [f for f in os.listdir(path_input) if osp.isfile(osp.join(path_input, f))]
+        n = len(files)
+        if self.verbose:
+            print(f'[FCRN]: Found {n} files in {path_input}')
 
+        if not osp.exists(path_output):
+            if self.verbose:
+                print('[FCRN]: Creating output directory {path_output} as it was not found.')
+            os.makedirs(path_output)
+
+        # create and setup h5 dataset file
+        depths = h5py.File(path_output, 'w')
+        depths.create_group('depths')
+        depths.create_dataset('names', data=files)
+
+        for i in range(n):
+            # load image
+            path_image = os.path.join(path_input, files[i])
+            image = self.load_image(path_image)
+
+            # estimate depth
+            pred = self.predict(image, sess)
+
+            # store depth into dataset
+            depths['depths'].create_dataset(files[i], data=pred)
+
+        depths.close()
+        return path_output
 
     def predict(self, image_cv2, sess):
         '''Evalute the network for the given image'''
         pred = sess.run(self.net.get_output(), feed_dict={self.input_node: image_cv2})
         return pred
 
+def predict_depth(path_model=FCRN_MODEL_PATH, path_input=FCRN_INPUT_PATH,
+                  path_output=FCRN_OUTPUT_PATH, verbose=False):
 
-def main():
+    fcrn = FCRNDepthPredictor(path_model, FCRN_IMAGE_SHAPE,
+                              channels=3, batch_size=1,
+                              verbose=verbose)
 
+    with tf.Session() as sess:
+        fcrn.load_weights(sess)
+
+        if args.verbose:
+            print('='*50 +'\n\n')
+
+        preds_path = fcrn.predict_dir(path_input, path_output, sess)
+        if verbose:
+            print(f'[FCRN]: Estimated depths are stored in {preds_path}')
+            print('[FCRN]: Done.')
+
+if __name__ == '__main__':
     def parse_args():
         """Parses arguments and returns args object to the main program"""
         parser = argparse.ArgumentParser()
@@ -136,24 +188,4 @@ def main():
     # parse arguments
     args, unknown = parse_args()
 
-    fcrn = FCRNDepthPredictor(args.model_path, args.image_path, args.predict_dir,
-                              (1024, 768), channels=3, batch_size=1, verbose=args.verbose)
-
-    with tf.Session() as sess:
-        fcrn.load_weights(sess)
-
-        if args.verbose:
-            print('='*50 +'\n\n')
-
-        # make predictions
-        if args.predict_dir:
-            fcrn.predict_dir(args.image_path)
-        else:
-            image = fcrn.load_image(args.image_path)
-            pred = fcrn.predict(image, sess)
-            fcrn.save_depth(pred, args.image_path + '_depth')
-
-    os._exit(0)
-
-if __name__ == '__main__':
-    main()
+    predict_depth(args.model, args.inp, args.out, args.verbose)
